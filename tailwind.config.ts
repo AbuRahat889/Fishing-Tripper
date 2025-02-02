9516b46f27ea8296bf3bdda98fbf211fb742a739
AbuRahat889 <aburahatshaum889@gmail.com>
2025-02-02 12:43:59 +0600

    setup this project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        textPrimary: "#242424",
+        textSecondary: "#171717",
+      },
+      container: {
+        center: true,
+        screens: {
+          DEFAULT: "1200px",
+        },
+      },
+      fontFamily: {
+        modernist: ["Sk-Modernist", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
       },
     },
   },
